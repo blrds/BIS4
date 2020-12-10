2e80cc349232a2bd4c77f6a9aaf9fc55d1a63bbc
blrds <blrds@yandex.ru>
2020-12-10 21:32:21 +0700

    Add project files.

---
+++ BIS4/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace BIS4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.h = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(13, 39);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(193, 23);
+            this.load.TabIndex = 1;
+            this.load.Text = "Загрузить";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(13, 68);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(193, 23);
+            this.start.TabIndex = 2;
+            this.start.Text = "Старт";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // h
+            // 
+            this.h.Location = new System.Drawing.Point(13, 13);
+            this.h.MaxLength = 4;
+            this.h.Name = "h";
+            this.h.Size = new System.Drawing.Size(193, 20);
+            this.h.TabIndex = 3;
+            this.h.Text = "adhh";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "1"});
+            this.listBox1.Location = new System.Drawing.Point(12, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(194, 277);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 384);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.h);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.TextBox h;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ BIS4/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BIS4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string text;
+        private void start_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            HashCalculator();
+        }
+
+        void HashCalculator()
+        {
+            const int k = 64, n=32;
+            ASCIIEncoding e = new ASCIIEncoding();
+            byte[] mass;
+            BitArray hi = new BitArray(e.GetBytes(h.Text));
+            BitArray m;
+            string str="";
+
+            for (int i = 0; i < text.Length/(k/8-n/8); i ++){
+                str = "";
+                for (int j = 0; j < n / 8; j++) {
+                    str += text[i*4 + j].ToString();
+                }
+                m = new BitArray(e.GetBytes(str));
+                hi.Xor(m);
+                hi = Shift(hi, 2);
+                hi.Xor(m);
+                mass = new byte[hi.Length/8];
+                hi.CopyTo(mass, 0);
+                str = "";
+                foreach (byte a in mass)
+                    str += (a.ToString()+" ");
+                listBox1.Items.Add(str);
+            }
+            listBox1.Items.Add("--");
+            listBox1.Items.Add(str);
+            }
+
+        static BitArray Shift(BitArray bitArray, int count)
+        {
+            for (int i = 0; i < count; i++) {
+                BitArray bit = new BitArray(1);
+                bit[0] = bitArray[bitArray.Count - 1];
+                for (int j = bitArray.Count - 1; j >= 1; j--)
+                    bitArray[j] = bitArray[j - 1];
+                bitArray[0] = bit[0];
+            }
+            return bitArray;
+        }
+        private void load_Click(object sender, EventArgs e)
+        {
+            string filePath = "";
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                //ofd.InitialDirectory = "c:\\";
+                ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                ofd.FilterIndex = 2;
+                ofd.RestoreDirectory = true;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //File f = new File(filePath);
+                    filePath = ofd.FileName;
+
+                    var fileStream = ofd.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        text = reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
